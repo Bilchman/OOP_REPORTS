05d7d812a8670550f34efef5f77fc55fd69fe370
taki <103680620+Bilchman@users.noreply.github.com>
2026-02-17 14:16:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+﻿function openLab(labId, element) {
+
+    // 1. Ховаємо всі лабораторні
+    document.querySelectorAll('.lab-section')
+        .forEach(sec => sec.classList.remove('active'));
+
+    // 2. Ховаємо всі підменю
+    document.querySelectorAll('.submenu')
+        .forEach(menu => menu.style.display = 'none');
+
+    // 3. Показуємо вибрану лабораторну
+    document.getElementById(labId)
+        .classList.add('active');
+
+    // 4. Показуємо відповідне підменю
+    element.nextElementSibling.style.display = 'block';
+
+    // 5. Прокручуємо сторінку вгору
+    window.scrollTo({ top: 0 });
+}
+// Функція для розгортання/згортання коду
+function toggleCode(header) {
+    const article = header.parentElement;
+    article.classList.toggle('open');
+}
+
+/* Автоматично відкриваємо першу лабораторну */
+document.querySelectorAll('.lab-title')[0].click();
